49f61347e4adafab6578f8c791433b4aa9ff46c1
Mohamed Amged <m.amged@emerging-media.co.uk>
2014-08-14 15:21:39 +0300

    +init elements

--- bs-tgl-btn.js
+++ bs-tgl-btn.js
@@ -1,7 +1,7 @@
 
     $('[data-toggle="toggle-btn"]').not('[readonly]').css('cursor', 'pointer').click(function(e) {
         function toggle(el) {
-            l($(el).attr('data-values'))
+            if($(el).attr('data-values')){
             var vals = classes = [],
                 values = $(el).attr('data-values').split(':'),
                 newValues = $(el).text() + ':' + $(el).attr('class');
@@ -10,8 +10,8 @@
                 .attr('title', values[1])
                 .attr('alt', values[1])
                 .attr('data-values', newValues);
-                l('*******************')
             toggle($($(el).attr('data-bind')).first());
+        }
         }
         toggle(this)
     });
