12241a5a306dd78649d57e080e54ec481a726573
Mohamed Amged <m.amged@emerging-media.co.uk>
2014-08-14 17:11:02 +0300

    +fix binding bidirectional

--- bs-tgl-btn.js
+++ bs-tgl-btn.js
@@ -1,17 +1,49 @@
+function l(m) {
+    console.log(m)
+}
+$('[data-toggle="toggle-btn"]').not('[readonly]').css('cursor', 'pointer').click(function(e) {
+    /*
+    if (!$(this).attr('toggled'))
+        $(this).attr('toggled', true);
+    else
+        $(this).removeAttr('toggled');
+    */
+    toggle(this)
+});
+var tog=false;
+function toggle(el) {
+    l('************')
+    l($(el))
+    if($(el).length){
+    var vals = classes = [],
+        values = $(el).attr('data-values').split(':'),
+        newValues = $(el).text() + ':' + $(el).attr('class');
+    $(el).text(values[0])
+        .attr('class', values[1])
+        .attr('title', values[1])
+        .attr('alt', values[1])
+        .attr('data-values', newValues);
+    l("$(el).attr('toggled'): "+ $(el).attr('toggled'))
+    l("$($(el).attr('data-bind')).first().attr('toggled')): " + $($(el).attr('data-bind')).first().attr('toggled'))
+    if (($(el).attr('toggled') ==   $($(el).attr('data-bind')).first().attr('toggled'))&& !tog) {
+        //l('true')
+        //l('el: ' + $(el))
+        //l('first' + $($(el).attr('data-bind')).first())
+        tog=true;
+        toggleElAttr(el)
+        toggleElAttr($($(el).attr('data-bind')).first());
+        toggle($($(el).attr('data-bind')).first());
+    }
+    else{
+        tog = false;
+        l(false)
+    }
+}
+}
 
-    $('[data-toggle="toggle-btn"]').not('[readonly]').css('cursor', 'pointer').click(function(e) {
-        function toggle(el) {
-            if($(el).attr('data-values')){
-            var vals = classes = [],
-                values = $(el).attr('data-values').split(':'),
-                newValues = $(el).text() + ':' + $(el).attr('class');
-            $(el).text(values[0])
-                .attr('class', values[1])
-                .attr('title', values[1])
-                .attr('alt', values[1])
-                .attr('data-values', newValues);
-            toggle($($(el).attr('data-bind')).first());
-        }
-        }
-        toggle(this)
-    });
+function toggleElAttr(el) {
+    if (!$(el).attr('toggled'))
+        $(el).attr('toggled', true);
+    else
+        $(el).removeAttr('toggled');
+}
