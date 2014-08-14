3a6c1148fee98869b669b5142295ecd3c584b627
Mohamed Amged <m.amged@emerging-media.co.uk>
2014-08-14 17:12:35 +0300

    prittyfy js

--- bs-tgl-btn.js
+++ bs-tgl-btn.js
@@ -1,36 +1,30 @@
 $('[data-toggle="toggle-btn"]').not('[readonly]').css('cursor', 'pointer').click(function(e) {
-    /*
-    if (!$(this).attr('toggled'))
-        $(this).attr('toggled', true);
-    else
-        $(this).removeAttr('toggled');
-    */
     toggle(this)
 });
-var tog=false;
+var tog = false;
+
 function toggle(el) {
-    if($(el).length){
-    var vals = classes = [],
-        values = $(el).attr('data-values').split(':'),
-        newValues = $(el).text() + ':' + $(el).attr('class');
-    $(el).text(values[0])
-        .attr('class', values[1])
-        .attr('title', values[1])
-        .attr('alt', values[1])
-        .attr('data-values', newValues);
-    if (($(el).attr('toggled') ==   $($(el).attr('data-bind')).first().attr('toggled'))&& !tog) {
-        //l('true')
-        //l('el: ' + $(el))
-        //l('first' + $($(el).attr('data-bind')).first())
-        tog=true;
-        toggleElAttr(el)
-        toggleElAttr($($(el).attr('data-bind')).first());
-        toggle($($(el).attr('data-bind')).first());
+    if ($(el).length) {
+        var vals = classes = [],
+            values = $(el).attr('data-values').split(':'),
+            newValues = $(el).text() + ':' + $(el).attr('class');
+        $(el).text(values[0])
+            .attr('class', values[1])
+            .attr('title', values[1])
+            .attr('alt', values[1])
+            .attr('data-values', newValues);
+        if (($(el).attr('toggled') == $($(el).attr('data-bind')).first().attr('toggled')) && !tog) {
+            //l('true')
+            //l('el: ' + $(el))
+            //l('first' + $($(el).attr('data-bind')).first())
+            tog = true;
+            toggleElAttr(el)
+            toggleElAttr($($(el).attr('data-bind')).first());
+            toggle($($(el).attr('data-bind')).first());
+        } else {
+            tog = false;
+        }
     }
-    else{
-        tog = false;
-    }
-}
 }
 
 function toggleElAttr(el) {
